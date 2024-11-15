482905ac0a0391a5d46eb5f8a86fae443ee060fe
bendan233 <166061381+bendan233@users.noreply.github.com>
2024-11-16 03:43:58 +0800

    Update domain_service.dart

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class DomainService {
   static const String ossDomain =
-      'https://v2.777158.xyz/on/websites.json';
+      'https://gitee.com/lanzimiaomiao/1fava1/releases/download/1svasd/websites.json';
 
 // 从返回的 JSON 中挑选一个可以正常访问的域名
   static Future<String> fetchValidDomain() async {
